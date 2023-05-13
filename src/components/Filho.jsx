b20e3945c5f6447f605f69434e58f6e460273b04
SP!RAL <mateuslimarod2602@gmail.com>
2023-05-13 18:27:44 -0300

    :)

---
+++ src/components/Filho.jsx
@@ -0,0 +1,14 @@
+const Filho = (falarComPai) => {   
+    const acaoBotao = () =>{
+        falarComPai("Oi Pai!")
+    } 
+    return(
+        <div>
+            <h1>Filho</h1>
+            <button onClick={ }>
+                ENVIAR MENSAGEM PRO PAI
+            </button>
+        </div>
+    )
+}
+export default Filho
