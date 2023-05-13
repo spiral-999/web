4e2f63c2bce95be30dc1abf0c2784b397de6604d
SP!RAL <mateuslimarod2602@gmail.com>
2023-05-13 18:31:44 -0300

    ´-´

---
+++ src/components/AULA01/HelloWorld.jsx
@@ -0,0 +1,39 @@
+// Versão 1 - Função Tradicional
+// function HelloWorld() {
+//   return (
+//     <div>
+//       <h1>Mateus Lima</h1>
+//       <h2>Ciências da Computação</h2>
+//     </div>
+//   );
+// }
+
+//Versão 2 - Função Seta
+const HelloWorld = () => {
+    return (
+      <div>
+        <h1>socorro</h1>
+      </div>
+    );
+  };
+  
+  //Versão 3 - Função Seta 2
+  // const HelloWorld = () =>
+  //   <div>
+  //     <h1>Mateus Lima</h1>
+  //     <h2>Ciências da Computação</h2>
+  //   </div>
+  
+  //Versão 4 - Classes
+  //import React from "react";
+  // class HelloWorld extends React.Component{
+  //   render(){
+  //     return(
+  //       <div>
+  //         <h1>Mateus Lima</h1>
+  //         <h2>Ciências da Computação</h2>
+  //       </div>
+  //     )
+  //   }
+  // }
+  export default HelloWorld;
