1df2360294e6238536e014c0cfa51f8255d1d305
SP!RAL <mateuslimarod2602@gmail.com>
2023-05-29 21:48:20 -0300

    ;)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Questao01 from './components/ATIVIDADES/Atividade04/Questão01/minhaPromise';
 import Questao02 from './components/ATIVIDADES/Atividade04/Questão02/MeuAsync';
 function App() {
   return (
